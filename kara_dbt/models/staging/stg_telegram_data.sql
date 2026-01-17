433c619c08a0109dd0efccab34c7d98e1adcff9d
Hermona Addisu <135220263+HermonaDev@users.noreply.github.com>
2026-01-17 15:03:28 +0300

    feat: finalize dbt pipeline and create the enrichment mart

--- kara_dbt/models/staging/stg_telegram_data.sql
+++ kara_dbt/models/staging/stg_telegram_data.sql
@@ -1,3 +1,4 @@
+-- models/staging/stg_telegram_data.sql
 with raw_data as (
     select * from {{ source('telegram_raw', 'telegram_messages') }}
 )
@@ -5,11 +6,10 @@ with raw_data as (
 select
     id as internal_id,
     channel,
-    (content->>'id')::int as message_id,
+    (content->>'id')::int as message_id, 
     (content->>'date')::timestamp as message_date,
     content->>'text' as message_text,
     (content->>'views')::int as views,
     (content->>'forwards')::int as forwards,
     content->>'image_path' as image_path
 from raw_data
-where content->>'text' is not null
